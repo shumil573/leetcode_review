f3901c49492c99060ad78b6ad6b539217f5c4d08
shumil573 <heshuyao@bupt.edu.cn>
2021-07-08 10:10:52 +0800

    Please enter the commit message for your changes. "1711+930"

---
+++ 1711.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   static constexpr int MOD = 1'000'000'007;
+    vector<int>& deliciousness={1,1,1,3,3,3,7};
+
+        int maxVal = *max_element(deliciousness.begin(), deliciousness.end());
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        unordered_map<int, int> mp;
+        int n = deliciousness.size();
+        for (auto& val : deliciousness) {
+            for (int sum = 1; sum <= maxSum; sum <<= 1) {
+                int count = mp.count(sum - val) ? mp[sum - val] : 0;
+                pairs = (pairs + count) % MOD;
+            }
+            mp[val]++;
+        }
+        cout<<pairs; 
+        return 0;
+}
+
+    
+    
---
+++ 930.cpp
@@ -0,0 +1,16 @@
+//930. 和相同的二元子数组
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum = 0;
+        unordered_map<int, int> cnt;
+        int ret = 0;
+        for (auto& num : nums) {
+            cnt[sum]++;
+            sum += num;
+            ret += cnt[sum - goal];
+        }
+        return ret;
+    }
+};
